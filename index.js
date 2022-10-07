2fb059ae31ef5a68f6c55f1225ed3fe39b551da0
Joseph Fabo <40829366+josephfabo@users.noreply.github.com>
2022-10-07 05:06:16 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+function btnfun() {
+	    var input = document.getElementById('userInput').value;
+		if (input.length == 0) {
+	      alert('input cant be empty');
+	    }else{
+	      document.getElementById('quote').innerHTML = "" + input;
+		  }
+	}function btnfun() {
+	    var input = document.getElementById('userInput').value;
+		if (input.length == 0) {
+	      alert('input cant be empty');
+	    }else{
+	      document.getElementById('quote').innerHTML = "" + input;
+		  }
+	}
